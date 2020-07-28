4dd1198e885a780cec5c9d5c083d4d045e2797af
Sunny <Sunny@Sunbirs-MacBook-Pro.local>
2020-07-28 02:00:31 -0700

    Third  Commit

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,20 +1,23 @@
-import React from "react";
-import UserItem from "./UserItem";
+import React from 'react';
 
-import "./UserList.css";
+import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
+import './UsersList.css';
 
-const UsersList = (props) => {
+const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
 
   return (
     <ul className="users-list">
-      {props.items.map((user) => (
+      {props.items.map(user => (
         <UserItem
           key={user.id}
           id={user.id}
