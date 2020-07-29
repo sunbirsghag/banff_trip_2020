b8e4f5188a03419111903914ee657153b9dafd2b
Sunny <Sunny@Sunbirs-MacBook-Pro.local>
2020-07-29 14:28:36 -0700

    auth

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -8,7 +8,7 @@ const DUMMY_PLACES = [
     title: "Banff",
     description: "Best Mountain Range in the World",
     imageUrl:
-      "https://www.google.com/search?q=banff+mountains&tbm=isch&source=iu&ictx=1&fir=Z52MBHHt_S1mWM%252CXdUhMmnpQN9U0M%252C_&vet=1&usg=AI4_-kTJaqBE19lc8gvlH9W3CnbuIZqybw&sa=X&ved=2ahUKEwia-uet6_DqAhUzKX0KHWLuD00Q9QEwAXoECAQQLg#imgrc=Z52MBHHt_S1mWM",
+      "https://www.planetware.com/photos-large/CDN/canada-banff-town-street.jpg",
     address: "Improvement District No. 9, AB",
     location: {
       lat: 51.1878337,
@@ -21,7 +21,7 @@ const DUMMY_PLACES = [
     title: "Jasper",
     description: "Second Best Mountain Range in the World",
     imageUrl:
-      "https://www.google.com/search?q=jasper+mountains&tbm=isch&source=iu&ictx=1&fir=QEdMQpAmmyIGAM%252Co3s-DOKZklHsvM%252C_&vet=1&usg=AI4_-kRgaP5IZJNVTt3pShrmCTR43XUEXg&sa=X&ved=2ahUKEwjJgra77PDqAhX4IDQIHVKcDxwQ9QEwCXoECAoQOQ#imgrc=QEdMQpAmmyIGAM",
+      "https://media.tacdn.com/media/attractions-splice-spp-674x446/07/0c/28/31.jpg",
     address: "Improvement District No. 12, AB T0E 1E0",
     location: {
       lat: 52.8798165,
